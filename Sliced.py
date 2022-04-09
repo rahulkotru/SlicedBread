6b30965c3cbd6683a9cc801210d726dcdbd15634
Rahul <rahulkotr1@gmail.com>
2022-04-09 16:08:59 +0530

    Defining methods

--- Sliced.py
+++ Sliced.py
@@ -2,8 +2,10 @@ import numpy as np
 import tifffile as tiff
 import os
 import matplotlib.pyplot as plt
+from PIL import Image
+Image.MAX_IMAGE_PIXELS=100000000000
 
-class Image():
+class Picture():
 
     def __init__(self):
         print("Please enter file directory")
@@ -16,22 +18,23 @@ class Image():
             print("Filename is: "+self.name)
 
 
-
-        
-
-    def tile(self):
-        print("This method will create tiles of dimension defined by the user")
-        print("The current dimension of the image is: "+ self.height + " x " + self.width)
-        print("Please enter valid tile dimensions(example: 225, 256, 512,1024)")
-
-
     def removeExt(self):
         base=os.path.basename(self.path)
         name=os.path.splitext(base)[0]
         self.name=name
+        
+
+    def tile(self,size):
+        #print("This method will create tiles of dimension defined by the user")
+        print("The current dimension of the image is:  {}  x  {}".format(self.height,self.width))
+        print("Please enter valid tile dimensions(example: 225, 256, 512,1024)")
+        tileSize=int(input())
 
 
 
-img=Image()
+
+
+
+img=Picture()
 img.tile()
 #D:\20_SanFrancisco\San Francisco\Flipped/T11.tif
