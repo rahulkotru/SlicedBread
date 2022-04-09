8a001ee2f9af3e1efb51918bc80960a4f8e9ebcb
Rahul <rahulkotr1@gmail.com>
2022-04-09 15:15:54 +0530

    Defining methods

--- Sliced.py
+++ Sliced.py
@@ -5,20 +5,30 @@ import matplotlib.pyplot as plt
 
 class Image():
 
-    def __init__(self,path):
-        self.path=path
-        mainImg=tiff.imread(self.path)
-        self.height,self.width=mainImg.shape
-        print(self.height)
-        print(self.width)
+    def __init__(self):
+        print("Please enter file directory")
+        self.path=input()
+        print(self.path)
+        if (os.path.exists(self.path)):
+            mainImg=tiff.imread(self.path)
+            self.height,self.width=mainImg.shape
+            self.removeExt()
+            print("Filename is: "+self.name)
 
+
+        
 
     def tile(self,row, col):
         self.row = row
         self.col = col
 
+    def removeExt(self):
+        base=os.path.basename(self.path)
+        name=os.path.splitext(base)[0]
+        self.name=name
 
 
 
-img=Image("D:/20_SanFrancisco/San Francisco/Flipped/T11.tif")
+img=Image()
+
 
