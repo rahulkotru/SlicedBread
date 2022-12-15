5d9830d66181b60dcbfaf1395ddabc1b285e76cc
Rahul <rahulkotr1@gmail.com>
2022-12-15 22:21:02 +0530

    Kilowatt test

---
+++ che.py
@@ -0,0 +1,10 @@
+from PIL import Image
+import numpy as np
+Image.MAX_IMAGE_PIXELS=100000000000
+import tifffile as tiff
+
+img1=Image.open("D:/28_GitHub/SlicedBread/Tester/Test.tif")
+print(img1.format)
+img2=np.asarray(img1)
+tiff.imsave("hi.tif",img2)
+12
